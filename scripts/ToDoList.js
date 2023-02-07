6a669a7681145d4858f61cd5d1a8ec1a6f861ce4
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-07 10:21:44 +0100

    fineshed task US-001

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -1,14 +1,12 @@
-const TASK_LIST_TITLE = 'Tasks';
-const PLACEHOLDER_TEXT = 'add task';
+import { TASK_LIST_TITLE } from './constants.js';
 
 export class ToDoList {
-  #tasks = ['1', '2', '3'];
-  placeForBord;
+  #tasks = [];
+  #placeForBord;
 
   constructor() {
-    this.placeForBord = document.querySelector('#toDoBoard');
+    this.#placeForBord = document.querySelector('#toDoBoard');
     this.render();
-    this.addListeners();
   }
 
   getTasks() {
@@ -23,23 +21,18 @@ export class ToDoList {
     this.#tasks = [...this.#tasks, taskItem];
   }
 
+  removeTask(index) {
+    this.#tasks.splice(index, 1);
+  }
+
   render() {
     const list = this.makeTitledListMarkUpWithClass(
       TASK_LIST_TITLE,
       this.getTasks(),
       'toDo'
     );
-    const input = this.makeInputWithPlaceholder(PLACEHOLDER_TEXT);
 
-    this.placeForBord.insertAdjacentHTML('beforeend', list);
-    this.placeForBord.insertAdjacentHTML('beforeend', input);
-  }
-
-  addListeners() {
-    this.placeForBord.addEventListener('click', (event) => {
-      event.preventDefault();
-      console.log(event.target);
-    });
+    this.#placeForBord.insertAdjacentHTML('beforeend', list);
   }
 
   makeTitledListMarkUpWithClass(title, array, className = '') {
@@ -54,17 +47,5 @@ export class ToDoList {
 
     return `<h2 class='${className && className + '__title'}'>${title}</h2>
     <ul class='${className && className + '__list'}'>${items}</ul>`;
-  }
-
-  makeInputWithPlaceholder(placeholder) {
-    return `
-    <form>
-      <label>
-      New task
-      <input type="text" name="task" placeholder='${placeholder}'/>
-      </label>
-    
-      <button type="submit">+</button>
-    </form>`;
   }
 }
