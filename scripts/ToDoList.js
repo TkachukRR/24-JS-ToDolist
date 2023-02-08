72f1ac2db87f316ae7f887062d9ba0221e9e703a
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-08 13:13:55 +0100

    #002 Added modal window and actions

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -5,6 +5,10 @@ import {
   INPUT_ADD_BUTTON_TEXT,
   INPUT_CHECK_REGEXP,
   BASE_DAYS_FOR_TASK,
+  MODAL_ADD_BUTTON_TEXT,
+  MODAL_CANCEL_BUTTON_TEXT,
+  MODAL_CREATED_DATE_DEF_VAL,
+  MODAL_EXPIRATION_DATE_DEF_VAL,
 } from './constants.js';
 
 export class ToDoList {
@@ -13,30 +17,9 @@ export class ToDoList {
 
   constructor() {
     this.#placeForBord = document.querySelector('#toDoBoard');
+
     this.render();
-
-    const input = this.#placeForBord.querySelector('[name="newTaskText"]');
-
-    input.addEventListener('input', (event) => {
-      !this.validate(event.target.value, INPUT_CHECK_REGEXP)
-        ? input.classList.add('bordered--red')
-        : input.classList.remove('bordered--red');
-    });
-
-    input.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        if (this.validate(event.target.value, INPUT_CHECK_REGEXP)) {
-          this.addTask({
-            task: input.value,
-            date: {
-              created: Date.now(),
-              expiration: Date.now() + BASE_DAYS_FOR_TASK * 24 * 3600 * 1000,
-            },
-          });
-        }
-      }
-    });
+    this.addEventListeners();
   }
 
   getTasks() {
@@ -59,6 +42,8 @@ export class ToDoList {
   }
 
   render() {
+    console.log(this.getTasks());
+
     const list = this.makeTitledListMarkUpWithClass(
       TASK_LIST_TITLE,
       this.getTasks(),
@@ -69,29 +54,46 @@ export class ToDoList {
       'newTaskForm',
       INPUT_LABLE
     );
+    console.log(list);
+
     this.#placeForBord.innerHTML = '';
 
     this.#placeForBord.insertAdjacentHTML('beforeend', list);
     this.#placeForBord.insertAdjacentHTML('beforeend', inputTask);
   }
 
+  addEventListeners() {
+    const input = this.#placeForBord.querySelector('[name="newTaskText"]');
+    const addBtn = this.#placeForBord.querySelector(
+      'button.newTaskForm__button'
+    );
+
+    input.addEventListener('input', this.onInputChange.bind(this));
+    input.addEventListener('keydown', this.onEnterBtn.bind(this));
+    addBtn.addEventListener('click', this.onAddBtn.bind(this));
+  }
+
   makeTitledListMarkUpWithClass(title, array, className = '') {
     if (!array) console.error('wait for array');
     if (!array.length) {
-      return `<h2 class='${
-        className && className + '__title'
-      }'>${title}</h2><p>SuperMan hasn't tasks.</p>`;
+      return `<h2 class='${className && className + '__title'}'>${title}</h2>
+      <ul class='${
+        className && className + '__list'
+      }'><li>SuperMan hasn't tasks.</li></ul>`;
     }
 
+    return `<h2 class='${className && className + '__title'}'>${title}</h2>
+    ${this.makeListMarkUpWithClass(array, className)}`;
+  }
+
+  makeListMarkUpWithClass(array, className = '') {
     const items = array
       .map(
         (item) =>
           `<li class='${className && className + '__item'}'>${item.task}</li>`
       )
       .join('');
-
-    return `<h2 class='${className && className + '__title'}'>${title}</h2>
-    <ul class='${className && className + '__list'}'>${items}</ul>`;
+    return `<ul class='${className && className + '__list'}'>${items}</ul>`;
   }
 
   makeInputMarkUpWithPlaceholderWithClass(placeholder, className = '', lable) {
@@ -110,7 +112,182 @@ export class ToDoList {
     </form>`;
   }
 
+  onInputChange(event) {
+    !this.validate(event.target.value, INPUT_CHECK_REGEXP)
+      ? event.target.classList.add('bordered--red')
+      : event.target.classList.remove('bordered--red');
+  }
+
+  onEnterBtn(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (this.validate(event.target.value, INPUT_CHECK_REGEXP)) {
+        this.addTask({
+          task: event.target.value,
+          date: {
+            created: Date.now(),
+            expiration: Date.now() + BASE_DAYS_FOR_TASK * 24 * 3600 * 1000,
+          },
+        });
+        this.rerenderTaskList();
+        event.target.value = '';
+      }
+      console.log(this.getTasks());
+    }
+  }
+
+  onAddBtn(event) {
+    event.preventDefault();
+
+    this.showModal();
+  }
+
   validate(inputString, regExp) {
     return !inputString || inputString.replace(regExp, '') === inputString;
+  }
+
+  rerenderTaskList() {
+    const newTasks = this.makeListMarkUpWithClass(this.getTasks(), 'toDo');
+    const taskList = this.#placeForBord.querySelector('.toDo__list');
+    taskList.innerHTML = newTasks;
+  }
+
+  makeModalMarkUp(innerMarkup) {
+    return `
+    <div class="backdrop">
+      <div class="modal">
+      <button class="modal__closed"></button>
+      <div class="modal__content">
+      ${innerMarkup}
+      </div>
+      </div>
+    </div>
+      `;
+  }
+
+  makeInnerModalMarkUp(
+    placeholder,
+    addBtnText,
+    cancelBtnText,
+    createdDate,
+    expirationDate
+  ) {
+    return `
+    <form >
+      <label>
+        Task
+        <input type="text" name="newTaskText" data-action="newTaskText" placeholder='${placeholder}'/>
+        <input type="date" name="createdDate" value='${createdDate}'/>
+        <input type="date" name="expirationDate" value='${expirationDate}'/>
+      </label>
+    </form>
+    <button class="add">${addBtnText}</button>
+    <button class="cancel">${cancelBtnText}</button>`;
+  }
+
+  showModal() {
+    const modal = this.makeModalMarkUp(
+      this.makeInnerModalMarkUp(
+        INPUT_PLACEHOLDER_TEXT,
+        MODAL_ADD_BUTTON_TEXT,
+        MODAL_CANCEL_BUTTON_TEXT,
+        MODAL_CREATED_DATE_DEF_VAL,
+        MODAL_EXPIRATION_DATE_DEF_VAL
+      )
+    );
+
+    this.#placeForBord.innerHTML = '';
+    this.#placeForBord.insertAdjacentHTML('beforeend', modal);
+
+    this.addModalListeners();
+  }
+
+  addModalListeners() {
+    console.log('addEventListeners');
+
+    const modal = this.#placeForBord.querySelector('.modal');
+    const modalClose = modal.querySelector('.modal__closed');
+    const modalInput = modal.querySelector('[data-action="newTaskText"]');
+    const modalAddBtn = modal.querySelector('button.add');
+    const modalCancelBtn = modal.querySelector('button.cancel');
+
+    modalClose.addEventListener('click', this.onModalClose.bind(this));
+    modalInput.addEventListener('input', this.onInputChange.bind(this));
+    modalInput.addEventListener('keydown', this.onModalEnterBtn.bind(this));
+    modalAddBtn.addEventListener('click', this.onModalAddBtn.bind(this));
+    modalCancelBtn.addEventListener('click', this.onModalCancelBtn.bind(this));
+  }
+
+  onModalClose(event) {
+    console.log('onModalClose');
+    this.render();
+    this.addEventListeners();
+    return;
+  }
+
+  onModalEnterBtn(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (this.validate(event.target.value, INPUT_CHECK_REGEXP)) {
+        console.log(event.target);
+        console.log(this.#placeForBord);
+
+        const taskText = this.#placeForBord.querySelector(
+          '[name="newTaskText"]'
+        ).value;
+        const createdDate = this.#placeForBord.querySelector(
+          '[name="createdDate"]'
+        ).value;
+        const expirationDate = this.#placeForBord.querySelector(
+          '[name="expirationDate"]'
+        ).value;
+
+        this.addTask({
+          task: taskText,
+          date: {
+            created: new Date(createdDate).getTime(),
+            expiration: new Date(expirationDate).getTime(),
+          },
+        });
+        event.target.value = '';
+      }
+    }
+  }
+
+  onModalAddBtn(event) {
+    if (
+      event.target.textContent === MODAL_ADD_BUTTON_TEXT &&
+      event.target.tagName === 'BUTTON'
+    ) {
+      event.preventDefault();
+      if (this.validate(event.target.value, INPUT_CHECK_REGEXP)) {
+        const taskText = this.#placeForBord.querySelector(
+          '[name="newTaskText"]'
+        ).value;
+        const createdDate = this.#placeForBord.querySelector(
+          '[name="createdDate"]'
+        ).value;
+        const expirationDate = this.#placeForBord.querySelector(
+          '[name="expirationDate"]'
+        ).value;
+
+        this.addTask({
+          task: taskText,
+          date: {
+            created: new Date(createdDate).getTime(),
+            expiration: new Date(expirationDate).getTime(),
+          },
+        });
+        this.#placeForBord.querySelector('[name="newTaskText"]').value = '';
+      }
+    }
+  }
+
+  onModalCancelBtn(event) {
+    this.#placeForBord.querySelector('[name="newTaskText"]').value = '';
+    this.#placeForBord.querySelector('[name="createdDate"]').value =
+      MODAL_CREATED_DATE_DEF_VAL;
+    this.#placeForBord.querySelector('[name="expirationDate"]').value =
+      MODAL_EXPIRATION_DATE_DEF_VAL;
   }
 }
--- scripts/constants.js
+++ scripts/constants.js
@@ -4,3 +4,13 @@ export const INPUT_LABLE = 'New task';
 export const INPUT_ADD_BUTTON_TEXT = '+';
 export const INPUT_CHECK_REGEXP = /[^A-za-z0-9]/;
 export const BASE_DAYS_FOR_TASK = 1;
+export const MODAL_ADD_BUTTON_TEXT = 'add';
+export const MODAL_CANCEL_BUTTON_TEXT = 'cancel';
+export const MODAL_CREATED_DATE_DEF_VAL = new Date(Date.now())
+  .toISOString()
+  .slice(0, 10);
+export const MODAL_EXPIRATION_DATE_DEF_VAL = new Date(
+  Date.now() + 24 * 3600 * 1000
+)
+  .toISOString()
+  .slice(0, 10);
