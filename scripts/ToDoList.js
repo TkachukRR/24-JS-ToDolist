a1dac85b0368b6c874f53f9915b85b33ece88400
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-08 09:13:38 +0100

    #002 Added Input with placeholder

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -1,7 +1,12 @@
-import { TASK_LIST_TITLE } from './constants.js';
+import {
+  TASK_LIST_TITLE,
+  INPUT_PLACEHOLDER_TEXT,
+  INPUT_LABLE,
+  INPUT_ADD_BUTTON_TEXT,
+} from './constants.js';
 
 export class ToDoList {
-  #tasks = [];
+  tasks = [];
   #placeForBord;
 
   constructor() {
@@ -10,21 +15,21 @@ export class ToDoList {
   }
 
   getTasks() {
-    return this.#tasks;
+    return this.tasks;
   }
 
   setTasks(tasksArray) {
-    this.#tasks = [...tasksArray];
+    this.tasks = [...tasksArray];
   }
 
   addTask(taskItem) {
-    this.#tasks = [...this.#tasks, taskItem];
+    this.tasks = [...this.tasks, taskItem];
   }
 
   removeTask(index) {
-    this.#tasks = [
-      ...this.#tasks.slice(0, index),
-      ...this.#tasks.slice(index + 1),
+    this.tasks = [
+      ...this.tasks.slice(0, index),
+      ...this.tasks.slice(index + 1),
     ];
   }
 
@@ -34,21 +39,49 @@ export class ToDoList {
       this.getTasks(),
       'toDo'
     );
+    const inputTask = this.makeInputMarkUpWithPlaceholderWithClass(
+      INPUT_PLACEHOLDER_TEXT,
+      'newTaskForm',
+      INPUT_LABLE
+    );
+    this.#placeForBord.innerHTML = '';
 
     this.#placeForBord.insertAdjacentHTML('beforeend', list);
+    this.#placeForBord.insertAdjacentHTML('beforeend', inputTask);
   }
 
   makeTitledListMarkUpWithClass(title, array, className = '') {
     if (!array) console.error('wait for array');
+    if (!array.length) {
+      return `<h2 class='${
+        className && className + '__title'
+      }'>${title}</h2><p>SuperMan hasn't tasks.</p>`;
+    }
 
     const items = array
       .map(
         (item) =>
-          `<li class='${className && className + '__item'}'>${item}</li>`
+          `<li class='${className && className + '__item'}'>${item.task}</li>`
       )
       .join('');
 
     return `<h2 class='${className && className + '__title'}'>${title}</h2>
     <ul class='${className && className + '__list'}'>${items}</ul>`;
+  }
+
+  makeInputMarkUpWithPlaceholderWithClass(placeholder, className = '', lable) {
+    return `
+    <form class='${className}' >
+      <label class='${className && className + '__lable'}' >
+      ${lable}
+      <input  class='${
+        className && className + '__input'
+      }' type="text" name="newTaskText" placeholder='${placeholder}'/>
+      </label>
+    
+      <button  class='${
+        className && className + '__button'
+      }' type="button">${INPUT_ADD_BUTTON_TEXT}</button>
+    </form>`;
   }
 }
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,4 @@
-export const TASK_LIST_TITLE = 'Tasks';
+export const TASK_LIST_TITLE = 'Tasks:';
+export const INPUT_PLACEHOLDER_TEXT = 'add task';
+export const INPUT_LABLE = 'New task';
+export const INPUT_ADD_BUTTON_TEXT = '+';
